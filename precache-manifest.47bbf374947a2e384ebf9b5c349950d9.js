f7c18c5d9a5b98618587b5e500b3cd0c4485bac1
rezaca <renato@rezaca.net>
2025-03-17 23:06:51 -0400

    Updates

---
+++ precache-manifest.47bbf374947a2e384ebf9b5c349950d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae13459ba177b8c8458b",
+    "url": "/static/css/main.21671dc5.chunk.css"
+  },
+  {
+    "revision": "ae13459ba177b8c8458b",
+    "url": "/static/js/main.720bb42a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d0a59c04e229d8e195e0",
+    "url": "/static/js/2.35033a49.chunk.js"
+  },
+  {
+    "revision": "0f9c2a14e3fe4a5baafc055c1006f2f8",
+    "url": "/index.html"
+  }
+];
