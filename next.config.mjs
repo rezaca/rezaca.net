d09587ac1907a1057b581d7d7b599bfd6fa76449
rezaca <renato@rezaca.net>
2025-03-26 10:34:27 -0400

    Optimize website for mobile devices

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   // Remove export for Vercel deployment
   images: {
     unoptimized: true,
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
+    formats: ['image/webp'],
   },
   webpack: (config) => {
     // Handle PDF loading through react-pdf
@@ -10,6 +13,9 @@ const nextConfig = {
     config.resolve.alias.encoding = false;
     
     return config;
+  },
+  experimental: {
+    optimizeFonts: true,
   },
 };
 
